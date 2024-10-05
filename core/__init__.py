7ef44c493dbecf9804f5e150258838c84fdbb2b6
Greg Wade <greg.r.wade@gmail.com>
2024-10-05 16:15:16 -0400

    flaskr tutorial is working

--- core/__init__.py
+++ core/__init__.py
@@ -1,42 +1,51 @@
-
 import os
+
 from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the flask application
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
-        pass        
+        pass
 
-    # a simple page that says hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
-    # initialize our db app portion
+    # register the database commands
     from . import db
+
     db.init_app(app)
 
-    # import our authentification blueprint
+    # apply the blueprints to the app
     from . import auth
+    from . import blog
+
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
 
-    # Initialize your app here
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
+
     return app
-
--- core/auth.py
+++ core/auth.py
@@ -10,9 +10,10 @@ from flask import session
 from flask import url_for
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
-from core.db import get_db
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+from .db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 def login_required(view):
@@ -30,14 +31,16 @@ def login_required(view):
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
 
 
 @bp.route("/register", methods=("GET", "POST"))
@@ -75,37 +78,39 @@ def register():
 
         flash(error)
 
-    return render_template("register.html")
+    return render_template("auth/register.html")
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
 
         if error is None:
+            # store the user id in a new session and return to the index
             session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
 
         flash(error)
 
-    return render_template('login.html')
+    return render_template("auth/login.html")
 
 
-@bp.route('/logout')
+@bp.route("/logout")
 def logout():
+    """Clear the current session, including the stored user id."""
     session.clear()
-    return redirect(url_for('index'))
-
+    return redirect(url_for("index"))
